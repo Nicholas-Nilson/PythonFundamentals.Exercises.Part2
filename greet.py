384253b64b57a65f9a95d1d09b71a35207d65f1f
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-06 19:52:31 -0400

    update

--- greet.py
+++ greet.py
@@ -2,11 +2,13 @@ def greet(name):
     """prints a greeting to the given name"""
     print("hey there " + name)
 
+
 def name_input():
     '''asks user for their name, for nefarious purposes later'''
     user_name = input("what's your name?\n")
     # greet(user_name)
     return user_name
+
 
 print("greet docstring: " + greet.__doc__)
 print("name input docstring: " + name_input.__doc__)
