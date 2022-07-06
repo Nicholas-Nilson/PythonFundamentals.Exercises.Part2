0a33faa638d8043f33f06d921aa838be1447252a
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-06 15:19:44 -0400

    changed method of printing results

--- exponentiator.py
+++ exponentiator.py
@@ -1,19 +1,19 @@
-def exponentiate(x, y):   #how else can this be printed w/o using print() in the function itself?
+def exponentiate(x, y):
     '''general case for raising a given number to a specified exponent'''
-    print(x**y)
+    return x**y
 
 def raise_to_the_fourth_power(x):
     '''get the 4th power of a given number'''
-    exponentiate(x, 4)
+    return exponentiate(x, 4)
 
 def square(x):
     '''get the square of a given number'''
-    exponentiate(x, 2)
+    return exponentiate(x, 2)
 
 def cube(x):
     '''get the cube of a given number'''
-    exponentiate(x, 3)
+    return exponentiate(x, 3)
 
-square(2)
-cube(2)
-raise_to_the_fourth_power(2)
+print(square(2))
+print(cube(2))
+print(raise_to_the_fourth_power(2))
